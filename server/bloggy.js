8715733ab99fbee06dc7eef8fda7333d09305199
Nathan Johnson <freakingjohnson@localhost.localdomain>
2018-02-28 04:07:24 -0700

    fasfdsf

--- server/bloggy.js
+++ server/bloggy.js
@@ -32,7 +32,7 @@ app.use(session({
 }))
 
 app.use(express.static(`${__dirname}/../build`))
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 })
 
